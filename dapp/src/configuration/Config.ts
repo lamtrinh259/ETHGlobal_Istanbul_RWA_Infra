e68973ca65680ba9562c4a22df6b8fdfb278f7f3
Theo Madzou <madztheo@hotmail.fr>
2023-11-19 06:32:19 +0300

    add gnosis to supported networks

--- dapp/src/configuration/Config.ts
+++ dapp/src/configuration/Config.ts
@@ -1,28 +1,49 @@
-import { ThemingProps } from '@chakra-ui/react'
-import { polygonMumbai, celoAlfajores, mantleTestnet, lineaTestnet, baseGoerli, arbitrumGoerli, scrollSepolia, mainnet, goerli, sepolia, polygon, optimism, arbitrum, scrollTestnet } from 'viem/chains'
+import { ThemingProps } from "@chakra-ui/react";
+import {
+  polygonMumbai,
+  celoAlfajores,
+  mantleTestnet,
+  lineaTestnet,
+  baseGoerli,
+  arbitrumGoerli,
+  scrollSepolia,
+  mainnet,
+  goerli,
+  sepolia,
+  polygon,
+  optimism,
+  arbitrum,
+  scrollTestnet,
+  gnosis,
+} from "viem/chains";
 
-export const SITE_NAME = 'AssetFuse'
-export const PROJECT_ID = '5f7aafd09a6666791de420f9025324fc'
-export const SITE_DESCRIPTION = 'Ready to Use Dapp Boilerplate'
-export const SITE_URL = 'eth-global-istanbul-rwa-infra.vercel.app'
+export const SITE_NAME = "AssetFuse";
+export const PROJECT_ID = "5f7aafd09a6666791de420f9025324fc";
+export const SITE_DESCRIPTION = "Ready to Use Dapp Boilerplate";
+export const SITE_URL = "eth-global-istanbul-rwa-infra.vercel.app";
 
-export const THEME_INITIAL_COLOR = 'system'
-export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
-export const THEME_CONFIG = { initialColorMode: THEME_INITIAL_COLOR }
+export const THEME_INITIAL_COLOR = "system";
+export const THEME_COLOR_SCHEME: ThemingProps["colorScheme"] = "gray";
+export const THEME_CONFIG = { initialColorMode: THEME_INITIAL_COLOR };
 
-export const SOCIAL_MEDIUM = '@SamuelQZQ'
-export const SOCIAL_TWITTER = '__Jalel'
-export const SOCIAL_GITHUB = 'SamuelQZQ'
-export const SOCIAL_LINKEDIN = 'SamuelQZQ'
-export const SOCIAL_DISCORD = '123456789123456789'
+export const SOCIAL_MEDIUM = "@SamuelQZQ";
+export const SOCIAL_TWITTER = "__Jalel";
+export const SOCIAL_GITHUB = "SamuelQZQ";
+export const SOCIAL_LINKEDIN = "SamuelQZQ";
+export const SOCIAL_DISCORD = "123456789123456789";
 
-export const INFURA_KEY = '817597f04d6941649c41255a1b10e815'
+export const INFURA_KEY = "817597f04d6941649c41255a1b10e815";
 export const NETWORKS = [
-    scrollSepolia,
-    arbitrumGoerli,
-    baseGoerli,
-    lineaTestnet,
-    mantleTestnet,
-    celoAlfajores,
-    polygonMumbai,
-    mainnet,]
+  scrollSepolia,
+  arbitrumGoerli,
+  baseGoerli,
+  lineaTestnet,
+  mantleTestnet,
+  {
+    ...celoAlfajores,
+    name: "Celo Alfajores",
+  },
+  polygonMumbai,
+  gnosis,
+  mainnet,
+];
